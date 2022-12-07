edbeeb940d9c9044ae00b283e821e9b8696e94bc
Paul Roll <57689893+paul-roll@users.noreply.github.com>
2022-12-07 15:31:28 +0000

    Added async and await functions - to return an array containing the image and its ID.

--- js/javascript.js
+++ js/javascript.js
@@ -2,6 +2,36 @@
 // Variables
 // ==========================================================================
 
+const imageUrlToBase64 = async url => {
+    const response = await fetch(url);
+    const blob = await response.blob();
+    return new Promise((onSuccess, onError) => {
+        try {
+            const imageID = response.headers.get("picsum-id");
+            const reader = new FileReader() ;
+            reader.readAsDataURL(blob) ;
+            reader.onload = function(){ onSuccess({"id":imageID,"image":this.result}) } ;
+        } catch(e) {
+            onError(e);
+        }
+    });
+};
+
+
+// ==========================================================================
+// Functions
+// ==========================================================================
+
+async function  getImage() {
+    return base64 = await imageUrlToBase64('https://picsum.photos/200/300');
+}
+
+async function showImage(base64) {
+    const array = await base64;
+    $("h1").after(`<img src=" ${array.image}" alt="">`);
+    $("img").after(`<p>Image ID: ${array.id}`);
+}
+
 
 // ==========================================================================
 // Core Events
@@ -11,16 +41,9 @@
 // ==========================================================================
 // Page Load
 // ==========================================================================
-$(document).ready(function(){
-        fetch('https://picsum.photos/200/300')
-        .then((response) => {
-            console.log(response);
-            $("h1").after(`<img src="${response.url}" alt="">`);
-            const imageID = response.headers.get("picsum-id");
-            $("img").after(`<p>${imageID}</p>`)
-            $("p").after(`<img src="https://picsum.photos/id/${imageID}/200/300" alt="">`)
-        });
-      
+
+$(document).ready(function(){     
+    showImage(getImage());
 });
 
 
